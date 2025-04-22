ad1a44344c337a33062e6ca6113d5588a0a2c12c
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-04-22 18:15:57 +0500

    Logger Middleware Added

--- app/store.js
+++ app/store.js
@@ -3,13 +3,17 @@ const configureStore = require('@reduxjs/toolkit').configureStore;
 const pizzaReducer = require('../features/pizza/pizzaSlice');  // import the reducer and assign it to a variable ( can be any name ) named pizzaReducer (default import)
 const burgerReducer = require('../features/burger/burgerSlice'); // import the reducer and assign it to a variable ( can be any name ) named burgerReducer (default import)
 
+const reduxLogger = require('redux-logger'); // import the redux logger
+const logger = reduxLogger.createLogger(); // createLogger is a function that takes in an object as an argument 
+
 // configureStore is a function that takes in an object as an argument
 const store = configureStore({
     // reducer is an object that contains the reducers of the application
     reducer : {
         pizza : pizzaReducer,   // Here key 'pizza' is the name of the slice and value pizzaReducer is the reducer of the slice that is imported above
         burger : burgerReducer, // Here key 'burger' is the name of the slice and value burgerReducer is the reducer of the slice that is imported above
-    }
+    },
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger) // middleware is an array that contains the middleware of the application
 });
 
 console.log(store); // Store contains the reducers of the application and combines them into a single object
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const store = require('./app/store'); // import the store from the app/store.js file
 const burgerActions = require('./features/burger/burgerSlice').burgerActions; // import the burgerActions from the features/burger/burgerSlice.js file
 const pizzaActions = require('./features/pizza/pizzaSlice').pizzaActions; // import the pizzaActions from the features/pizza/pizzaSlice.js file
-//const pizzaReducer = require('./features/pizza/pizzaSlice').reducer; // import the pizzaReducer from the features/pizza/pizzaSlice.js file
-//const pizzaSlice = require('./features/pizza/pizzaSlice'); // import the pizzaSlice from the features/pizza/pizzaSlice.js file
-console.log("First State: " ,store.getState()); // getState() is a function that returns the current state of the application
+
+// console.log("First State: " ,store.getState()); // getState() is a function that returns the current state of the application
 const unsubscribe = store.subscribe(() => {
-    console.log("Updated State: ",store.getState()); // getState() is a function that returns the current state of the application
+   // console.log("Updated State: ",store.getState()); // getState() is a function that returns the current state of the application
+
 });
 
 store.dispatch(pizzaActions.pizza_order(15));
 store.dispatch(burgerActions.burger_order(5));
 
 unsubscribe();
-console.log("First State: " ,store.getState());
+
 // store.dispatch is a function that takes in an action as an argument
 // action is an object that contains the type and payload properties
 // type is the name of the action
